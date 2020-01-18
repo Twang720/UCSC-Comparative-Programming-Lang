0278f140d83d16b837d66de40c292268ee58bfec
Twang720 <timqwang@gmail.com>
2020-01-18 15:31:48 -0800

    Added documentation and started on evaluate-expression

--- Assignment1/sbi.scm
+++ Assignment1/sbi.scm
@@ -1,6 +1,10 @@
 #!/afs/cats.ucsc.edu/courses/cse112-wm/usr/racket/bin/mzscheme -qr
 ;; $Id: sbi.scm,v 1.19 2020-01-14 16:58:47-08 - - $
 ;;
+;; STUDENTS
+;;    Timothy Wang tqwang
+;;    Eric Mar emmar
+;;
 ;; NAME
 ;;    sbi.scm - silly basic interpreter
 ;;
@@ -12,12 +16,14 @@
 ;;    program, which is the executed.  Currently it is only printed.
 ;;
 
+;; Given - Defines consts for stdin, stdout, stderr, and arg-list
 (define *stdin* (current-input-port))
 (define *stdout* (current-output-port))
 (define *stderr* (current-error-port))
 (define *arg-list* (vector->list (current-command-line-arguments)))
 
 
+;; Hash-table with all functions needed for lookup
 (define *function-table* (make-hash))
 
 (for-each
@@ -54,6 +60,7 @@
 
 (define *variable-table* (make-hash))
 	 
+;; Defines pi, e, non-numbers, and (?) end of file
 (for-each
     (lambda (pair) 
         (hash-set! *variable-table* (car pair) (cadr pair)))
@@ -85,10 +92,16 @@
 
 (define (interpret-input (lambda (mem) ))
 
+;; evaluates the expression lmao
 (define (evaluate-expression f))
+    ;; if it is a number, return it
+    (if (number? f) (+ f 0.0))
+    ;; if it is a symbol in the table, look it up then return it
+    (if (variable? f) (+ (hash-ref *variable-table* f 0) 0.0) 
+    ;; TODO: if it's anything else
 
 
-
+;; Given - defines run file
 (define *run-file*
     (let-values
         (((dirpath basepath root?)
@@ -96,30 +109,36 @@
         (path->string basepath))
 )
 
+;; Given - for when given an error, exits
 (define (die list)
     (for-each (lambda (item) (display item *stderr*)) list)
     (newline *stderr*)
     (exit 1)
 )
 
+;; Given - exit out of program with error statement
 (define (usage-exit)
     (die `("Usage: " ,*run-file* " filename"))
 )
 
+;; Given - reads list from input file
 (define (readlist-from-inputfile filename)
     (let ((inputfile (open-input-file filename)))
+         ;; If it's not the right type, errors, otherwise reads it
          (if (not (input-port? inputfile))
              (die `(,*run-file* ": " ,filename ": open failed"))
              (let ((program (read inputfile)))
                   (close-input-port inputfile)
                          program))))
 
+;; Given - reads token from stdin?? not too sure
 (define (dump-stdin)
     (let ((token (read)))
          (printf "token=~a~n" token)
          (when (not (eq? token eof)) (dump-stdin))))
 
 
+;; Given - prints out what program is doing
 (define (write-program-by-line filename program)
     (printf "==================================================~n")
     (printf "~a: ~a~n" *run-file* filename)
@@ -128,15 +147,16 @@
     (for-each (lambda (line) (printf "~a~n" line)) program)
     (printf ")~n"))
 
+;; Given - Main Function
 (define (main arglist)
+    ;; Check to make sure exactly 1 input, assumes sbir file
     (if (or (null? arglist) (not (null? (cdr arglist))))
         (usage-exit)
         (let* ((sbprogfile (car arglist))
                (program (readlist-from-inputfile sbprogfile)))
               (write-program-by-line sbprogfile program))))
 
-(if (terminal-port? *stdin*)
-    (main *arg-list*)
-    (printf "sbi.scm: interactive mode~n"))
+;; Given - runs main
+(main *arg-list*)
 
 
