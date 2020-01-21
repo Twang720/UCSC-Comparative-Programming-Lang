1b817b1869a59b2b8eecdd2cbf3f14e995a00d91
Twang720 <timqwang@gmail.com>
2020-01-20 22:16:20 -0800

    formatting for string

--- Assignment1/sbi.scm
+++ Assignment1/sbi.scm
@@ -189,19 +189,19 @@
       (if (null? prints)
         (printf "~n")
         ;; checks if its a string
-        (begin
-        (if (string? (car prints))
-              (display (car prints))
-              ;; if not then must be expression
-              (display (eval-expr (car prints))))
-        (interpret-print (cdr prints)))))
+
+            (if (string? (car prints))
+                (display (car prints))
+                ;; if not then must be expression
+            (display (eval-expr (car prints)))))
+      (when (not (null? prints))
+          (interpret-print (cdr prints))))
 
 ;; first argument of the <mems> list is the key//address (?) rest are the values to store
 (define (interpret-input mems)
   (if (null? mems)
       (exit 1)
-  (display "WIP"))
-)
+  (display "WIP~n")))
 
 
 ;; Given - defines run file (?)
